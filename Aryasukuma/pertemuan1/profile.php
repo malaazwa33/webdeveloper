2f7221f1dc11e755c8e5c68869e43e3cab99fe85
Aryaksuma <aryakusuma1278@gmail.com>
2020-09-27 07:39:12 -0700

    uu

--- Aryasukuma/pertemuan1/profile.php
+++ Aryasukuma/pertemuan1/profile.php
@@ -36,7 +36,7 @@
   <center>
   <body>
   <body bg-color="lightblue">
-       <h3 class="text-center text-success mt-3" style="color: rgb(); font-weight:bold">PROFILE PEMBUAT</h3></a>
+       <h3 class="text-center text-success mt-3" style="color: rgb(0,0,0); font-weight:bold">PROFILE PEMBUAT</h3></a>
 
         <!-- Isi Biodata Paling Keren di HTML disini -->
 <div class="container">
@@ -55,34 +55,34 @@
         <table border="0" width="100%" style="padding-left: 2px; padding-right: 13px;">
           <tbody>
             <tr>
-              <td width="25%" valign="top" class="textt" style="color: rgb(); font-weight:bold">Nama</td>
+              <td width="25%" valign="top" class="textt" style="color: rgb(0,0,0); font-weight:bold">Nama</td>
                 <td width="2%">:</td>
-                <td style="color: rgb(9); font-weight:bold">Arya Kusuma</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">Arya Kusuma</td>
             </tr>
           <tr>
-              <td class="textt" style="color: rgb(); font-weight:bold">Jenis Kelamin</td>
+              <td class="textt" style="color: rgb(0,0,0); font-weight:bold">Jenis Kelamin</td>
                 <td>:</td>
-                <td style="color: rgb(); font-weight:bold">Laki-Laki</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">Laki-Laki</td>
             </tr>
           <tr>
-              <td class="textt" style="color: rgb(); font-weight:bold">Tempat Lahir</td>
+              <td class="textt" style="color: rgb(0,0,0); font-weight:bold">Tempat Lahir</td>
                 <td>:</td>
-                <td style="color: rgb(); font-weight:bold">Jakarta</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">Jakarta</td>
             </tr>
           <tr>
-              <td class="textt" style="color: rgb(); font-weight:bold">Tanggal Lahir</td>
+              <td class="textt" style="color: rgb(0,0,0); font-weight:bold">Tanggal Lahir</td>
                 <td>:</td>
-                <td style="color: rgb(); font-weight:bold">11 Maret 2002</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">11 Maret 2002</td>
             </tr>
           <tr>
-              <td class="textt" style="color: rgb(); font-weight:bold">Fakultas</td>
+              <td class="textt" style="color: rgb(0,0,0); font-weight:bold">Fakultas</td>
                 <td>:</td>
-                <td style="color: rgb(); font-weight:bold">Teknik</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">Teknik</td>
             </tr>
           <tr>
-              <td valign="top" class="textt" style="color: rgb(); font-weight:bold">Prodi</td>
+              <td valign="top" class="textt" style="color: rgb(0,0,0); font-weight:bold">Prodi</td>
                 <td valign="top">:</td>
-                <td style="color: rgb(); font-weight:bold">Teknik Informatika</td>
+                <td style="color: rgb(0,0,0); font-weight:bold">Teknik Informatika</td>
             </tr>
             <tr>
             <td>    <a class="link" href="index.php">Kembali</a></td>
