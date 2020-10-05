5d68f320aa9f6cd5a446688da305f4c8fb485b16
Aryaksuma <aryakusuma1278@gmail.com>
2020-10-04 21:50:05 -0700

    qq

---
+++ Aryasukuma/ars/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <h1>LOGIN BRAY</h1>
+
+    <div class="col-md-5">
+        <label>Username :</label>
+        <input type="text" class="form-control" name="" placeholder="Masukan Nama Anda">
+    </div>
+
+    <div class="col-md-5">
+        <label>Password :</label>
+        <input type="password" class="form-control" name="" placeholder="Masukan Password">
+    </div>
+    <br>
+
+    <div class="col-md-5">
+        <a href="" class="btn btn-primary">Login</a>
+    </div>
+    </div>
+   
+</body>
+</html>
