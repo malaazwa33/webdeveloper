c914f55cdd2a0220b6fa73b03bedab4045a99d2f
Wahyudevlombok <44743502+wahyutaqdirulaziz@users.noreply.github.com>
2020-10-09 07:29:29 -0700

    commit pertma

---
+++ dasarhtml/index.php
@@ -0,0 +1,63 @@
+<html>     
+<head>
+<title>belajar</title>
+<style>
+    body{
+        background-image: url('../dasarhtml/img/tes.jpg');
+        width: 9000%;
+  height: auto;
+    }
+
+    h1{
+        color: red;
+    }
+
+    .bebas{
+        background-color: yellow;
+        margin-top: 50px;
+       
+    }
+
+    #tes{
+        color: blue ;
+        text-align: center;
+        font-family: fantasy;
+        
+    }
+    .p1{
+     color: yellow;
+    }
+
+    .p2{
+
+    color:pink ;
+    background-color: blue;
+    }
+
+
+
+</style>
+</head>
+
+<body>
+   
+    <div class="bebas" >
+    <h1 id="tes">hello</h1>
+<button style="color: red; background-color: blue;">kirim</button>
+
+    <h3>helo3</h3>
+    </div>
+    <h1>hello world</h1>
+    <br>
+    <h2>hello kuy</h2>
+    <marquee behavior="" direction=""><p class="p1">hallo semuanya yang ada di sini</p></marquee>
+    <p class="p2">this paragrap</p>
+    <img src="https://pbs.twimg.com/media/DAGOyhPUAAEdRyc.j" width="50%" height="60%" alt="ini poto jeng oppa">
+    <a href="https://www.w3schools.com" target="_blank">Visit W3Schools</a>
+    <a href="www.youtube.com"> ini yang gak ada atribut target blank nya </a>
+</body>
+
+ 
+
+
+</html>
---
+++ wahyu/table.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tabel</title>
+</head>
+<body>
+
+<table   border="1">
+        <tr>
+        <th>nama</th>
+        <th>alamat</th>
+        <th>nomer</th>
+        </tr>
+
+        <tr>
+            <td>tes</td>
+            <td>jalan mulu jadian kagak</td>
+            <td>087768768</td>
+        </tr>
+
+</table>
+<br>
+<br>
+<center>
+<form action="">
+    <label for="">nama</label>
+    <br>
+    <input type="text" placeholder="masukan nama anda">
+    <br>
+    <label for="">alamat</label>
+    <br>
+   <select name="" id="">
+       <option value="">lombok barat</option>
+       <option value="">lombok tengah</option>
+       <option value="">lombok timur</option>
+   </select>
+    <br>
+    <label for="">nomer hp</label>
+    <br>
+    <input type="text">
+    <br>
+    <br>
+    <button>kirim</button>
+
+</form>
+
+</center>
+
+<iframe src="/webdeveloper/wahyu/table.php" frameborder="0"></iframe>
+
+
+<iframe width="420" height="315"
+src="https://www.youtube.com/embed/pZ9YL_rHEio">
+</iframe>
+
+
+<audio controls>
+  <source src="horse.ogg" type="audio/ogg">
+  <source src="horse.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+
+    
+</body>
+</html>
