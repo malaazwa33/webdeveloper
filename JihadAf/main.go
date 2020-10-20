56349295d7d3ef148f7b00004178994f37966729
jihad22 <jihad.0622@gmail.com>
2020-10-21 00:08:21 +0700

    Go, struct

---
+++ JihadAf/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+)
+
+//Car : struct => for saving data Car
+type Car struct {
+	Merk  string
+	Speed int
+	Type  string
+}
+
+//main : run code
+func main() {
+	//append data onto Car Struct
+	c := Car{
+		Merk:  "Toyota Supra",
+		Speed: 7,
+		Type:  "v-10.1",
+	}
+	//print data Car from struct
+	log.Printf("Car Merk : %s\n", c.Merk)
+	log.Printf("Car Speed : %d\n", c.Speed)
+	log.Printf("Car Type: %s", c.Type)
+}
